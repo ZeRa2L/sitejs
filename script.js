32903f4d20a00662f2308e17e9c2537c91c30a8e
ZeRa2L <tofindaio@gmail.com>
2021-09-11 17:41:50 +0300

    начал попап на джаваскрипт

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* крутая 3д картинка */
+
 const cards = document.querySelectorAll('.card');
 
 for (let i = 0; i < cards.length; i++) {
@@ -19,3 +21,41 @@ function stopRotate(event) {
     const cardItem = this.querySelector('.card-item');
     cardItem.style.transform = 'rotate(0)';
 }
+
+/* крутая 3д картинка */
+
+/* попап */
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('.body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener("click", function (e) {
+            const popupName = popupLink.gerAttribute('href').replace('#', '');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefoult();
+        });
+    }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupCloseIcon(el.closest('.popup'));
+        });
+    }
+}
+
+
+
+/* попап */
